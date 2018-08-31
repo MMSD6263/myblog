7011435b23b3b5831339cc4a6dd819c68af1153b
zhongxiang <fromzhongxiang@163.com>
2018-08-31 18:44:48 +0800

    123

---
+++ mb/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+
+class category(models.Model):
+    category_name = models.CharField(max_length = 50)
+    create_at = models.DateTimeField()
+
+
+class tag(models.Model):
+    tag_name = models.CharField(max_length = 50)
+    create_at = models.DateTimeField()
+
+class articles(models.Model):
+    key_word = models.CharField(max_length=255)
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    cid = models.IntegerField()
+    tags = models.CharField(max_length = 255)
+    create_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
